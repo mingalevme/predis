c6948a3d4ba1a39b881c7924714881c322c42036
Mingalev Mikhail <mingalevme@gmail.com>
2016-06-23 17:41:54 +0300

    Methods are now commented

--- src/Client.php
+++ src/Client.php
@@ -4,26 +4,62 @@ namespace Mingalevme\Predis;
 
 class Client extends \Predis\Client
 {
+    /**
+     * Insert all the specified values at the tail of the list stored at key.
+     * If key does not exist, it is created as empty list before performing the push operation. 
+     * 
+     * @param string $key
+     * @param string $value
+     * @return int The length of the list after the push operations.
+     */
     public function zrpush(string $key, string $value)
     {
         return $this->zadd($key, 'NX', microtime(true), $value);
     }
     
+    /**
+     * Insert all the specified values at the head of the list stored at key.
+     * If key does not exist, it is created as empty list before performing the push operations.
+     * 
+     * @param string $key
+     * @param string $value
+     * @return int The length of the list after the push operations.
+     */
     public function zlpush(string $key, string $value)
     {
         return $this->zadd($key, 'NX', -microtime(true), $value);
     }
     
+    /**
+     * Removes and returns the first element of the list stored at key.
+     * 
+     * @param string $key
+     * @return string|null
+     */
     public function zlpop($key)
     {
         return $this->zpop($key, 0, 0);
     }
     
+    /**
+     * Removes and returns the last element of the list stored at key.
+     * 
+     * @param string $key
+     * @return string|null
+     */
     public function zrpop($key)
     {
         return $this->zpop($key, -1, -1);
     }
     
+    /**
+     * Removes and returns the range of elements between $start and $end.
+     * 
+     * @param type $key
+     * @param int $start
+     * @param int $end
+     * @return string|null
+     */
     protected function zpop($key, $start, $end)
     {
         while ($result = $this->zrange($key, $start, $end)) {
